ce3389dfc091c72e343120136d5b08027d2f95e7
happy-baby0303 <tlswltjs27222@gmail.com>
2026-05-29 17:21:36 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const questionDB = [
+    { id: 1, category: "첫인사", question: "오늘 {babyName}의 표정 중 가장 기억에 남는 것은 무엇인가요?" },
+    { id: 2, category: "감정", question: "{babyName}를 재우고 나서, 새근새근 자는 모습을 보며 드는 생각은?" },
+    { id: 3, category: "관찰", question: "오늘 {babyName}가 가장 크게 웃었던 순간은 언제인가요?" },
+    { id: 4, category: "위로", question: "오늘 하루, 육아하느라 고생한 나에게 해주고 싶은 따뜻한 한마디는?" },
+    { id: 5, category: "미래", question: "{babyName}가 커서 엄마/아빠의 어떤 점을 쏙 닮았으면 좋겠나요?" }
+];
